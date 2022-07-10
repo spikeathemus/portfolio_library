046aa327e97bdf03dcedc88c5d7266dc32fa05d7
michal turocha <michalturocha@gmail.com>
2022-07-10 10:15:41 +0200

    Initial commit

---
+++ src/main/java/com/portfolio/webapp/portfolio_library/PortfolioLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.webapp.portfolio_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/webapp/portfolio_library/PortfolioLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.webapp.portfolio_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
